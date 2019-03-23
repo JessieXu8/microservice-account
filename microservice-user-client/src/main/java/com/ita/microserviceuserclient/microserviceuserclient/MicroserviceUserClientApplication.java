7330f17125bab5e7250cb785a70137fccf5245f5
Jessie Xu <jessie.xu@oocl.com>
2019-03-23 17:35:12 +0800

    micro service user client - other function

--- microservice-user-client/src/main/java/com/ita/microserviceuserclient/microserviceuserclient/MicroserviceUserClientApplication.java
+++ microservice-user-client/src/main/java/com/ita/microserviceuserclient/microserviceuserclient/MicroserviceUserClientApplication.java
@@ -3,10 +3,17 @@ package com.ita.microserviceuserclient.microserviceuserclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceUserClientApplication {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceUserClientApplication.class, args);
