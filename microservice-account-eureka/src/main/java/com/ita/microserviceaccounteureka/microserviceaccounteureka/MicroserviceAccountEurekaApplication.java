9339e23663d630fe81e8366800f863fb9eb771ba
Jessie Xu <jessie.xu@oocl.com>
2019-03-23 13:19:21 +0800

    microservice eureka server

---
+++ microservice-account-eureka/src/main/java/com/ita/microserviceaccounteureka/microserviceaccounteureka/MicroserviceAccountEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.ita.microserviceaccounteureka.microserviceaccounteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceAccountEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAccountEurekaApplication.class, args);
+    }
+
+}
