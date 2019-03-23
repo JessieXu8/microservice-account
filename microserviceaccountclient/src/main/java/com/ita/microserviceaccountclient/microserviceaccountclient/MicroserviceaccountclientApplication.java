28bd9ff6a53f2f2c250646b3d6d16054366e0ab2
Terry Teng <terry.teng@oocl.com>
2019-03-23 17:42:25 +0800

    account client add annotation

--- microserviceaccountclient/src/main/java/com/ita/microserviceaccountclient/microserviceaccountclient/MicroserviceaccountclientApplication.java
+++ microserviceaccountclient/src/main/java/com/ita/microserviceaccountclient/microserviceaccountclient/MicroserviceaccountclientApplication.java
@@ -2,11 +2,13 @@ package com.ita.microserviceaccountclient.microserviceaccountclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceaccountclientApplication {
 @Bean
 public RestTemplate restTemplate(){
